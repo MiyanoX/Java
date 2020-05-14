b5c3afdf89d5b622f8db3b4372f61eb82673a21e
MiyanoX <603460267@qq.com>
2020-05-14 20:37:06 +0900

    5-14

---
+++ Test/MathTest.java
@@ -0,0 +1,8 @@
+public class MathTest {
+    public static void main(String[] args) {
+        double a = Math.random();
+        double b = Math.random();
+        System.out.println("a = " + a + "; b = " + b);
+        System.out.println("bigger one in a & b is " + Math.max(a, b));
+    }
+}
